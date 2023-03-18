e95a5c4a1d80b1128f70af77a94957314e862849
Mafika <rudolphmafika@gmail.com>
2023-03-18 09:25:28 -0700

    fixed another typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, lsd);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lsd);
 	}
 
 	return (0);
