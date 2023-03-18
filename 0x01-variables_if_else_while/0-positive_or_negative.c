90c434411eab48800b007cea66538eb6142c59e1
Mafika <rudolphmafika@gmail.com>
2023-03-17 17:55:42 -0700

    cheking errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	}
 	if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 
 	}
 
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 
 	}
 
