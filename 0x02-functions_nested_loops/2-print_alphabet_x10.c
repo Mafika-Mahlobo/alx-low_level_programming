7456712c6ed3ea31222ac7dd15af3a9d55ac8872
Mafika <rudolphmafika@gmail.com>
2023-03-21 10:06:32 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet x10 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int n, i;
+
+	for (n = 1; n < 11; n++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
