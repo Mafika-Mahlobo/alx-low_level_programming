9ab56b00f31e737652d78ee0671c8f574f3847aa
Mafika <rudolphmafika@gmail.com>
2023-03-21 10:44:07 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _islower - check lowercase characters
+*
+* @c: user input (character)
+*
+* Return: Always 0 (Success)
+*
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
