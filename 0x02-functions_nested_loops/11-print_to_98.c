6733a63426a31e0ae269df4cf65c6ce52789b966
Mafika <rudolphmafika@gmail.com>
2023-03-22 11:37:22 -0700

    task 11 error fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
-		_putchar(n);
-		if (n == 98)
+		if (n < 98)
 		{
-			continue;
+			printf("%d, ", n);
 		}
-		_putchar(',');
-		_putchar(' ');
+		else
+		{
+			printf("%d", n)
+		}
 		_putchar('\n');
-		n++;
 	}
 }
