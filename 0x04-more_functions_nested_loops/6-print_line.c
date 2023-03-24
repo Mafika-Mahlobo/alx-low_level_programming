23ed590979515ab62b3f34633874ae67a8b78797
Mafika <rudolphmafika@gmail.com>
2023-03-24 06:27:24 -0700

    task 6 error fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n < 1)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
