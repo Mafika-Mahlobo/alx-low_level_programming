41b407d11bec26ab9b6bc9e31de968754287047b
Mafika <rudolphmafika@gmail.com>
2023-03-23 19:24:24 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0 t 9
+* Return: No return value
+*
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
