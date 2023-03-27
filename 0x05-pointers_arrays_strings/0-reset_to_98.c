5cd98452d2d50ebbdccc5025bb23d5e23af75817
Mafika <rudolphmafika@gmail.com>
2023-03-27 11:50:56 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - set variable poited to by poiter to 98
+*
+*@n: poiter (user input)
+* Return: No return value
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
