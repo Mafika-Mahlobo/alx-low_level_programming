cd8f6091fb20a14adbd565229aa38050cd158eea
Mafika <rudolphmafika@gmail.com>
2023-04-12 11:48:31 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+* _strdup -  returns pointer to memory address if string
+*@str: string
+* Return: Always 0
+*/
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 0, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	p = malloc(sizeof(char) * i);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (; str[r]; r++)
+		p[r] = str[r];
+
+	return (p);
+}
