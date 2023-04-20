4bd352fe978309f5798749b3816eca7ac22d327e
Mafika <rudolphmafika@gmail.com>
2023-04-20 11:17:42 -0700

    task 1 initial commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* print_numbers - prints number using variadic func
+* @separator: string to separate numbers
+* @n: number of integers
+* Return: Always 0
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int val = va_arg(args, int);
+
+		printf("%d", val);
+		if (separator != NULL && i < (n - 1))
+			printf("%s ", separator);
+
+	}
+	printf("\n");
+}
+
