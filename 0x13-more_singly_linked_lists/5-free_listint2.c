e56df87f4834ceaef617100c2fbfc2201fccd711
Mafika <rudolphmafika@gmail.com>
2023-05-03 15:40:20 -0700

    task 5 initial commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint2 - frees every note in a linked list
+* @head: pointer to first node if liked list
+* Return: Always 0
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
