1217e61b7f190de592d9e1217ab7285c35839a01
Mafika <rudolphmafika@gmail.com>
2023-05-05 19:21:24 -0700

    task 5 initial commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* flip_bits - return number of bits needed to flip to get to anoner number
+* @n: integer value
+* @m: number of bits
+* Return: Alwys 0
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	return (0);
+}
