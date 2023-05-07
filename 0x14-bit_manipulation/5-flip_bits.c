22fc359f0302ff6b1574336164f187c8808251f5
Mafika <rudolphmafika@gmail.com>
2023-05-06 17:47:33 -0700

    edited comment

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,7 +6,7 @@
 * flip_bits - return number of bits needed to flip to get to anoner number
 * @n: integer value
 * @m: number of bits
-* Return: Alwys 0
+* Return: Always 0
 */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
