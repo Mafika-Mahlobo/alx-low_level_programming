6c4eff782012f2563577fb1331070d928741c52f
Mafika <rudolphmafika@gmail.com>
2023-05-09 11:37:43 -0700

    task 1 error fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	flags = O_CREAT | O_WRONLY | O_TRUNC;
-	mode = S_IRUSR | S_IWUSR
+	mode = S_IRUSR | S_IWUSR;
 
 	descriptor = open(filename, flags, mode);
 
