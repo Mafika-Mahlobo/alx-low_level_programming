7828f80f173d73c61394eb7c73d0652232815774
Mafika <rudolphmafika@gmail.com>
2023-10-06 07:30:05 -0700

    edited task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned int i;
 
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
@@ -16,6 +17,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_table_t *) * size);
+
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
