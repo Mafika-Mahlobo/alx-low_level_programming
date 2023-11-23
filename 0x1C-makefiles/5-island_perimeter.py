b0ea0565fc289a1707c2b86ff9a6a5f830ff6038
Mafika <rudolphmafika@gmail.com>
2023-11-22 19:11:54 -0800

    fixed errors, task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,5 +22,5 @@ def island_perimeter(grid):
                 if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1i
+                    edges += 1
     return size * 4 - edges * 2
